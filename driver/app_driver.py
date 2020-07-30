8a3bcc18c87e1c73c39651a708cd001f5e32eda9
dujian <575758259@qq.com>
2020-07-30 14:30:52 +0800

    第一次提交

--- driver/app_driver.py
+++ driver/app_driver.py
@@ -9,7 +9,10 @@ def appdriver():
         'deviceName': '127.0.0.1:62001',
         'platformVersion': '5.1.1',
         'appPackage': 'cn.missfresh.application',
-        'appActivity': 'cn.missfresh.module.base.main.view.SplashActivity'
+        'appActivity': 'cn.missfresh.module.base.main.view.SplashActivity',
+        'noReset': 'True',
+        'unicodeKeyboard': 'True',
+        'resetKeyboard': 'True'
     }
     app_driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities)
     return app_driver #返回驱动
